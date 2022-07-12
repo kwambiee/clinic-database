15614d30f23fd8a8b9ed9f8da83d897b717d5323
kwambiee <kwambokaj2.jk@gmail.com>
2022-07-12 15:26:57 +0300

    Remove unecessary files

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,6 +1,5 @@
 /* Database schema to keep the structure of entire database. */
 
-/* Database schema to keep the structure of entire database. */
 
 CREATE TABLE medical_histories (
     ID INT PRIMARY KEY,
