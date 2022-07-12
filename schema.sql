4b1a4430186fa9ba4582941916bf8152295e3ff3
kwambiee <kwambokaj2.jk@gmail.com>
2022-07-12 15:24:16 +0300

    Add tables to clinic

--- schema.sql
+++ schema.sql
@@ -1,5 +1,18 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+/* Database schema to keep the structure of entire database. */
+
+CREATE TABLE medical_histories (
+    ID INT PRIMARY KEY,
+    ADMITTED_AT TIMESTAMP,
+    PATIENT_ID INT,
+    STATUS VARCHAR);
+
+CREATE TABLE patients (ID INT PRIMARY KEY,name VARCHAR,Date_of_Birth DATE);
+
+CREATE TABLE treatments (id INT PRIMARY KEY,type VARCHAR,name VARCHAR);
+CREATE TABLE invoices (id INT PRIMARY KEY,total_amount DECIMAL,generated_at TIMESTAMP,payed_at TIMESTAMP,medical_history_id INT);
+
+CREATE TABLE invoice_items (id INT PRIMARY KEY,unit_price DECIMAL,quantity INT,total_price DECIMAL,invoice_id INT,treatment_id INT);
+
+
